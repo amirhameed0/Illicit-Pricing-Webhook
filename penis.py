69653498e693afad0195e4c76e58fe0abd3613bc
amirhameed0 <72898355+amirhameed0@users.noreply.github.com>
2020-10-14 22:01:49 -0400

    Create penis.py

---
+++ penis.py
@@ -0,0 +1,40 @@
+{
+  "username": " Illicit Bot",
+  "embeds": [
+    {
+      "author": {
+        "name": "PRICE LIST", 
+        "url": "",
+        "icon_url": "https://toppng.com/uploads/preview/cash-symbol-11550729488jphztgwjrd.png"
+      },
+      "color": "64121",
+      "fields": [
+        {
+          "name": "Logos - 20$",
+          "value": "→Custom logo in high resolution PNG or JPEG file.",
+          "inline": false
+        },
+        {
+          "name": "",
+          "value": "",
+          "inline": false
+        },
+        {
+          "name": "",
+          "value" : "",
+         "inline": false
+        }
+      ],
+      "thumbnail": {
+        "url": ""
+      },
+      "image": {
+        "url": ""
+      },
+      "footer": {
+        "text": "",
+        "icon_url": ""
+      }
+    }
+  ]
+}
